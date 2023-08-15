733abd65a3e3a174c005e20c372d27bfe0dc2f3c
cain413 <140903105+cain413@users.noreply.github.com>
2023-08-15 12:40:55 -0400

    Create main.js

---
+++ main.js
@@ -0,0 +1,17 @@
+let secretMessage = ['Learning', 'is', 'not', 'about', 'what', 'you', 
+'get', 'easily', 'the', 'first', 'time,', 'it', 'is', 'about', 'what',
+ 'you', 'can', 'figure', 'out.', '-2015,', 'Chris', 'Pine,', 'Learn', 'JavaScript'];
+
+ secretMessage.pop('JavaScript');
+
+ const count = secretMessage.push('to', 'program');
+
+secretMessage[7] = 'right';
+
+secretMessage.shift(0);
+
+ let = secretMessage.unshift('Programming');
+
+ secretMessage.splice(6,5);
+
+console.log(secretMessage);
